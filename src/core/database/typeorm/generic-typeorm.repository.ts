5e3753cbc2847a78fcb970ac9b50c2c95b34e92b
wonjjee <yoteamo7@naver.com>
2024-02-08 20:52:39 +0900

    feat: added GenericTypeOrmRepository

---
+++ src/core/database/typeorm/generic-typeorm.repository.ts
@@ -0,0 +1,76 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  EntityTarget,
+  FindOneOptions,
+  Repository,
+  SelectQueryBuilder,
+} from 'typeorm';
+import { TransactionManager } from './transaction.manager';
+import { ClassConstructor, plainToInstance } from 'class-transformer';
+import { RootEntity } from './root.entity';
+
+@Injectable()
+export abstract class GenericTypeOrmRepository<T extends RootEntity> {
+  protected abstract readonly txManager: TransactionManager;
+  constructor(private readonly classType: ClassConstructor<T>) {}
+
+  async insertMany(models: T[]): Promise<T[]> {
+    const results = await this.getRepository().save(models);
+
+    return results.map((res) => plainToInstance(this.classType, res));
+  }
+
+  abstract getName(): EntityTarget<T>;
+
+  async findOneOrThrow(filters: Partial<T>): Promise<T> {
+    const findOption: FindOneOptions = { where: filters };
+    const res = this.getRepository().findOne(findOption);
+
+    if (!res) {
+      throw new BadRequestException(`don't exist ${findOption}`);
+    }
+    return plainToInstance(this.classType, res);
+  }
+
+  async findByIdOrThrow(id: number): Promise<T> {
+    const findOption: FindOneOptions = { where: { id } };
+    const res = await this.getRepository().findOne(findOption);
+
+    if (!res) {
+      throw new BadRequestException(`don't exist ${id}`);
+    }
+    return plainToInstance(this.classType, res);
+  }
+
+  async update(models: T): Promise<T> {
+    const res = await this.getRepository().save(models);
+
+    return plainToInstance(this.classType, res);
+  }
+
+  async updateMany(models: T[]): Promise<T[]> {
+    const results = await this.getRepository().save(models);
+
+    return results.map((res) => plainToInstance(this.classType, res));
+  }
+
+  async createEntity(model: T): Promise<T> {
+    const res = await this.getRepository().save(model);
+    return plainToInstance(this.classType, res);
+  }
+
+  async deleteById(id: number) {
+    return this.getRepository().softDelete(id);
+  }
+
+  protected getRepository(): Repository<T> {
+    return this.txManager.getEntityManager().getRepository(this.getName());
+  }
+
+  protected getQueryBuilder(): SelectQueryBuilder<T> {
+    return this.txManager
+      .getEntityManager()
+      .getRepository(this.getName())
+      .createQueryBuilder(String(this.getName()).toLowerCase());
+  }
+}
